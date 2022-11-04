b41534511aead4d074cdbe3a091979dedc8c2d3b
R0ckwe11 <r0ckwe112233@gmail.com>
2022-11-05 00:53:16 +0100

    add basic entities and schemas

--- feel_good_inc_app/lib/redis.js
+++ feel_good_inc_app/lib/redis.js
@@ -7,3 +7,55 @@ async function connect() {
         await client.open(process.env.REDIS_URL);
     }
 }
+
+class User extends Entity {}
+class Event extends Entity {}
+class Review extends Entity {}
+class Category extends Entity {}
+
+let userSchema = new Schema(
+    User, {
+        id: {type: 'number'},
+        name: {type: 'text'},
+        isInstitution: {type: 'boolean'},
+        description: {type: 'text'},
+    },
+    {
+        dataStructure: 'JSON',
+    }
+);
+
+let eventSchema = new Schema(
+    Event, {
+        id: {type: 'number'},
+        name: {type: 'text'},
+        org: {type: 'number'},
+        description: {type: 'text'},
+        image: {type: 'string'},
+        city: {type: 'text'},
+        address: {type: 'text'},
+        categories: {type: 'string[]'},
+    },
+    {
+        dataStructure: 'JSON',
+    }
+);
+
+let reviewSchema = new Schema(
+    Review, {
+        eventId: {type: 'number'},
+        review: {type: 'number'},
+    },
+    {
+        dataStructure: 'JSON',
+    }
+);
+
+let categorySchema = new Schema(
+    Category, {
+        name: {type: 'string'},
+    },
+    {
+        dataStructure: 'JSON',
+    }
+);
