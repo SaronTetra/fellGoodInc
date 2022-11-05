32094b6b1d5d1788df241730bd29a0a558d59a55
Adam Przywuski <adam.przywuski@nexio.pl>
2022-11-05 05:23:20 +0100

    fix linter warning'

--- feel_good_inc_app/src/pages/index.tsx
+++ feel_good_inc_app/src/pages/index.tsx
@@ -216,7 +216,7 @@ function ModalNewElement(): JSX.Element {
                    </div>
                    <div className="flex space-x-2">
                        <label htmlFor="description" className="py-10 block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Description</label>
-                       <textarea  name="description" id="description"  rows="5"  className="bg-gray-50 border width=20% border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500" ></textarea>
+                       <textarea  name="description" id="description"  rows={5}  className="bg-gray-50 border width=20% border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500" ></textarea>
                    </div>
         
        </div>
