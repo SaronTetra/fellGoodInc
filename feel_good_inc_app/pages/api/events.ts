c53d6d6d208d36ce8debaab2c2d370e86ab76a86
Maciej Stosik <starwars.maciek@gmail.com>
2022-11-05 04:23:04 +0100

    Fix routing

--- feel_good_inc_app/pages/api/events.ts
+++
@@ -1,6 +0,0 @@
-import { getAllEvents } from "../../lib/redis";
-
-export default async function handler(_req: any, res: any) {
-  const events = await getAllEvents();
-  res.status(200).json({ events });
-}
